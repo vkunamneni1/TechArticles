a5a3ac1a3b1f43393a267a9f1b4a95d69fa8f0b4
Veda Kunamneni <vedakunamneni@Vedas-MacBook-Air.local>
2025-05-30 22:46:56 -0400

    added source files

---
+++ articles.js
@@ -0,0 +1,38 @@
+var articles = [
+  {
+    image: "https://miro.medium.com/max/2712/1*T2CzKMo8aU02pxQmcYS4Bg.png",
+    title: "How Netflix works: the (hugely simplified) complex stuff that happens every time you hit Play",
+    description: "What isn't as simple is what goes into running Netflix, a service that streams around 250 million hours of video per day to around 98 million paying subscribers in 190 countries. At this scale, providing quality entertainment in a matter of a few seconds to every user is no joke. ",
+    link: "https://medium.com/refraction-tech-everything/how-netflix-works-the-hugely-simplified-complex-stuff-that-happens-every-time-you-hit-play-3a40c9be254b"
+  },
+  {
+    image: "https://miro.medium.com/max/4096/1*1XEF9NuNQy0rSu4kVdbb6A.jpeg",
+    title: "How the Web Works: A Primer for Newcomers to Web Development (or anyone, really)",
+    description: `If you're just getting into web development, chances are you think you know how the web works - at least on a basic level. ...But then you try to explain how a basic website works and draw a blank. What does an IP address mean again? How does the "client-server" model work, exactly?`,
+    link: "https://medium.com/free-code-camp/how-the-web-works-a-primer-for-newcomers-to-web-development-or-anyone-really-b4584e63585c?source=search_post"
+  },
+  {
+    image: "https://miro.medium.com/max/2798/0*0YMuiC1ZXqY5Q8bj.png",
+    title: "Designing User Interfaces for My Mother",
+    description: "It happens about once or twice a year. I travel over to my mother's house for a visit and, about two or three hours in, she says something like 'Hey, can you take a look at my computer? I just want to make sure everything is okay with it. You know I don't know what I'm doing.' She's right. She doesn't know what she's doing. She wants to, though.",
+    link: "https://medium.com/@TGines/designing-user-interfaces-for-your-mother-dd45ec50f7b0"
+  },
+  {
+    image: "https://miro.medium.com/max/2000/1*ofMfrqFMfpaTH_RMvI_RQA.jpeg",
+    title: "Learning To Code By Writing Code Poems",
+    description: `Back in 2008, I started studying design and clearly remember how the sheer sight of code just intimidated me. I had some coding classes, and without any prior coding experience, I had a tough time understanding the code I first got in touch with. Suddenly, the words that I was used to in the English language (such as "new" "return" and "throw/catch") took on a whole new meaning; the cryptic syntax, semicolons, brackets and completely new rules didn't make things any easier for me.`,
+    link: "https://www.smashingmagazine.com/2018/07/writing-code-poems/"
+  },
+  {
+    image: "https://miro.medium.com/max/1400/1*Y8Ge56YmDyPcK8FJrYQxKA.jpeg",
+    title: "Do You Know How Search Engine Works? ",
+    description: "As we all know by now, the web is flooded with information. It has tons (and tons) of websites, information, content, video, images, ect, that makes us really happy to look at. Now, if you think about it for just a moment, there needs to be some type of order behind the scenes, so when users type in a keyword (or several keywords), they will receive precise results (thus, leading them to feel super satisfied). Correct? Correct!",
+    link: "https://medium.com/@heatherluvsjesus/do-you-know-how-search-engine-works-ab7625a6e3ab"
+  },
+  {
+    image: "https://miro.medium.com/max/2000/1*odG2LJMa7L_5FnRm73w8NA.png",
+    title: "Light or Dark UI? Tips to Choose a Proper Color Scheme for User Interface",
+    description: `Our everyday life is an endless flow of choices. In both personal and professional life, we have to consider numerous oppositions and challenges, and itâ€™s better when solutions are based on not only suggestions but also facts, experience and knowledge. Today we are going to discuss one of the frequent choices UI designers have to make project by project: which scheme is better for the interface, light or dark?`,
+    link: "https://uxplanet.org/light-or-dark-ui-tips-to-choose-a-proper-color-scheme-for-user-interface-9a12004bb79e"
+  }
+]
